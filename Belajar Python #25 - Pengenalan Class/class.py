a6f526ae8b45a9e59946a184bdca3e8ee8ddd7e6
IhsanDevs <ichsanhafizdalfajry@gmail.com>
2021-04-16 18:42:10 +0700

    -

---
+++ Belajar Python #25 - Pengenalan Class/class.py
@@ -0,0 +1,10 @@
+class mahasiswa():
+  nama = "Ini adalah nama"
+  
+nama_1 = mahasiswa()
+nama_2 = mahasiswa()
+nama_1.nama = 'Nanda'
+nama_2.nama = 'Budi'
+print(nama_1.nama)
+print(nama_2.nama)
+
